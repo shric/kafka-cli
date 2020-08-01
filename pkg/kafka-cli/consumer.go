11d269c8d4996bc5a0772adfc077c11d46b7c62a
Chris Young <chris@chris.net.au>
2020-08-01 21:54:22 +1000

    Add comments

--- pkg/kafka-cli/consumer.go
+++ pkg/kafka-cli/consumer.go
@@ -3,15 +3,18 @@ package kafka_cli
 import (
 	"context"
 	"fmt"
-	"github.com/segmentio/kafka-go"
 	"io"
+
+	"github.com/segmentio/kafka-go"
 )
 
+// ConsumerCmd represents the consumer subcommand
 type ConsumerCmd struct {
 	Group string
 	Topic string
 }
 
+// Run executes the consumer subcommand
 func (c ConsumerCmd) Run(out io.Writer) error {
 	client := kafka.NewClient("localhost:9092")
 	ctx := context.Background()
